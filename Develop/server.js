79baa5b6d8f21da0e81685a6615c4ef572548d6f
Chase <chambers.chase@gmail.com>
2023-05-29 13:11:25 -0400

    Temp code - WOrking on heroku

--- Develop/server.js
+++ Develop/server.js
@@ -1,20 +1,48 @@
 const express = require('express');
 const path = require('path');
 const app = express();
+const noteItems = require('./db/db.json')
 const PORT = 3001;
 
 app.listen(PORT,
     () => console.log(`server is running on ${PORT}`));
 
 app.use(express.static('public'));
-app.use(express.json());
 
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '/public/index.html'));
+});
 
-app.get('*', (req, res) => 
-    res.sendFile(path.join(__dirname, 'public/index.html'))
-    );
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public/notes.html'))
+});
 
+app.get('/api/notes', (req, res) => res.json(noteItems));
 
-app.get('/notes', (req, res) => 
-  res.sendFile(path.join(__dirname, 'public/notes.html'))
-  );
+app.post('/api/notes', (req, res) => {
+    // Log that a POST request was received
+    console.info(`${req.method} request received to add a note`);
+  
+    // Destructuring assignment for the items in req.body
+    const { title, text } = req.body;
+  
+    // If all the required properties are present
+    if (title && text) {
+      // Variable for the object we will save
+      const newNote = {
+        title,
+        text,
+        review_id: uuid(),
+      };
+  
+      const response = {
+        status: 'success',
+        body: newNote,
+      };
+  
+      console.log(response);
+      res.status(201).json(response);
+    } else {
+      res.status(500).json('Error in posting review');
+    }
+  });
