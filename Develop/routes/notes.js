fe5c8c5eb6458f58ab261e63d28e15c591ba74bf
Chase <chambers.chase@gmail.com>
2023-05-29 19:48:14 -0400

    System not reading id

--- Develop/routes/notes.js
+++ Develop/routes/notes.js
@@ -6,10 +6,12 @@ const fs = require('fs');
 
 let noteDatabase = fs.readFileSync('./db/db.json');
 
-// GET Route for retrieving all the tips
+// GET Route for retrieving all the notes
 notes.get('/notes', (req, res) => {
   readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)));
 });
+
+
 
 // POST Route for a new note
 notes.post('/notes', (req, res) => {
@@ -31,30 +33,19 @@ notes.post('/notes', (req, res) => {
   }
 });
 
-notes.delete('api/notes/:id', (req, res) => {
-  const id = req.params.id * 1;
-  const noteToDelete = noteDatabase.find(el => el.id === id);
-  console.log(id);
-  console.log(noteToDelete);
+notes.delete('/notes/:id', (req, res) => {
+  
+const id = req.params.id*1;
+const index = noteDatabase.indexOf(id);
 
-  if (!noteDatabase) {
-    return res.status(404).json({
-      status:'fail',
-      message: `No note object with ID ${id} to delete.`
-    })
-  };
+if (req.params.id === id) {
+  const newNote = {};
+readAndAppend(newNote, './db/db.json');
+    res.json(`Note added successfully 🚀`);
+  } else {
+    res.error('Error in adding Note');
+  }
 
-  const index = noteDatabase.indexOf(noteToDelete);
-  noteDatabase.splice(index, 1);
-
-  fs.writeFile('./db/db.json', JSON.stringify(noteDatabase), (err) => {
-    res.status(204).json({
-      status: "success",
-      data: {
-        newNote: null
-      }
-    })
-  })
 });
 
 module.exports = notes;
